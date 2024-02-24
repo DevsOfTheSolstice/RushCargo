6d2554d207b3950874109ff786fa16e3f19dd333
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-02-24 14:47:48 -0400

    feat: Added SQL Sentence to Create Vehicle Description Table (Spanish Version)

---
+++ AllCars/createTables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Descripcion_Vehiculo ( 
+    id BIGSERIAL NOT NULL PRIMARY KEY, 
+    marca VARCHAR(50) NOT NULL, 
+    fecha_lanzamiento DATE NOT NULL, 
+    modelo VARCHAR(50) NOT NULL, 
+    tipo_alimentacion VARCHAR(50) NOT NULL, 
+    capacidad_watts INT, 
+    cantidad_cilindros INT, 
+    litros_motor FLOAT 
+);

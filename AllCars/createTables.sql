76dd7f934e2c46df39e896340a37ba3551a411a5
JeJaMel <jjmelendezb15@gmail.com>
2024-02-24 15:40:57 -0400

    feat: Added SQL Sentence to Create Ente, CLientes/natural/juridico.

--- AllCars/createTables.sql
+++ AllCars/createTables.sql
@@ -18,3 +18,30 @@ CREATE TABLE Vehiculo (
     matricula VARCHAR(50) NOT NULL,
     FOREIGN KEY (id_descripcion) REFERENCES Descripcion_Vehiculo(id)
 );
+
+CREATE TABLE Ente (
+    id BIGSERIAL PRIMARY KEY,
+    telefono VARCHAR(15) NOT NULL,
+    direccion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Clientes (
+    id BIGSERIAL PRIMARY KEY,
+    id_ente BIGINT,
+    FOREIGN KEY (id_ente) REFERENCES Ente(id)
+);
+
+CREATE TABLE Cliente_Natural (
+    ci BIGINT PRIMARY KEY,
+    id_cliente BIGINT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id)
+);
+
+CREATE TABLE Cliente_Juridico (
+    rif VARCHAR(15) PRIMARY KEY,
+    id_cliente BIGINT,
+    nombre VARCHAR(50) NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id)
+); 

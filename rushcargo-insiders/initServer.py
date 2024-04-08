385e2adbcfa34865731eea748a12ef281f219b3e
kaucrow <kaucrow@protonmail.com>
2024-04-08 10:36:01 -0400

    feat: Added `etc_queries.sql`

---
+++ rushcargo-insiders/initServer.py
@@ -0,0 +1,15 @@
+import subprocess
+
+# Path to the Python Interpreter that runs any Python Script under the Virtual Environment
+pythonBin = "./venv/Scripts/python.exe"
+
+# Path to the Python Script that must run under the Virtual Environment
+scriptFile = "./src/app.py"
+
+try:
+    # Open Script File
+    subprocess.run([pythonBin, scriptFile])
+
+except Exception as err:
+    print(err)
+    input("Press ENTER to Exit")
